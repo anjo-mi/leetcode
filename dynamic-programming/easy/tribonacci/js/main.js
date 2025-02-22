89a94b1a041e4c5c753e71ff2f7bf4b41f6108bc
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-22 17:31:41 -0500

    find nth occurrence in a tribonacci sequence

--- dynamic-programming/easy/tribonacci/js/main.js
+++ dynamic-programming/easy/tribonacci/js/main.js
@@ -1,1 +1,21 @@
+/*
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    let init = 0;
+    let secInit = 1;
+    let thirdInit = 1;
+    if (n < 3){
+        return n === 2 || n === 1 ? 1 : 0;
+    }
+    let curr = 0;
+    for (let i = 3 ; i <= n ; i++){
+        curr = init + secInit + thirdInit;
+        init = secInit;
+        secInit = thirdInit;
+        thirdInit = curr;
+    }
+    return curr;
+};
 
