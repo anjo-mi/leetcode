5aaf7ab1a617b5d9470c40cdcba1f578914921b7
kyle <nojaim.k@gmail.com>
2025-03-03 23:55:36 -0500

    n plus m time complexity rather than exponential

--- arrays/merge-2d-arrays/js/main.js
+++ arrays/merge-2d-arrays/js/main.js
@@ -1,14 +1,17 @@
+
 /*
  * @param {number[][]} nums1
  * @param {number[][]} nums2
  * @return {number[][]}
  */
 var mergeArrays = function(nums1, nums2) {
-    for (let i = 0 ; i < nums1.length ; i++){
-        const id = nums1[i][0]
-        if (nums2.some(pair => pair[0] === id)){
-            nums1[i][1] += nums2[nums2.findIndex(el => el[0] === id)][1]
-        }
+    const totals = {};
+    const merged = [];
+    for (const nest of nums1.concat(nums2)){
+        totals[nest[0]] = ( totals[nest[0]] || 0 ) + nest[1];
     }
-    return nums1.concat(nums2.filter(pair => !nums1.some(group => group[0] === pair[0]))).sort((a,b) => a[0] - b[0])
+    for (const total in totals){
+        merged.push([+total, totals[total]]);
+    }
+    return merged;
 };
