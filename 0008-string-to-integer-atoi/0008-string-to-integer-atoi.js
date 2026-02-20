6d674ff75e6fc20b875bace8bb8f643371d572bb
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-20 16:04:32 -0500

    Time: 92 ms (5.09%), Space: 64.1 MB (5.09%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    s = s.trim();
+    const sign = s[0] === '-' ? -1 : 1;
+    if (sign < 0 || s[0] === '+') s = s.slice(1);
+    const digits = new Set('0123456789.');
+    let res = '';
+    let hasDecimal = false;
+    for (let i = 0 ; i < s.length ; i++){
+        const char = s[i];
+        console.log({res,char})
+        if (!res && char === '0') continue;
+        if (!digits.has(char)) break;
+        if (char === '.' && hasDecimal) break;
+        if (char === '.') hasDecimal = true;
+        res += char;
+    }
+    res = Number(res);
+    if (res >= 2**31){
+        res = 2**31;
+        res += sign >= 0 ? -1 : 0;
+    }
+    return sign >= 0 ? Math.round(res) : Math.round(-1 * res);
+};
