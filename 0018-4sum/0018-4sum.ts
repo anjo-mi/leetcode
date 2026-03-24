8576f0bfc75ac207c900036b515c38f11a105e1c
Kyle Nojaim <nojaim.k@gmail.com>
2026-03-24 11:53:56 -0400

    Time: 28 ms (25.2%), Space: 64.1 MB (9.28%) - LeetHub

--- 0018-4sum/0018-4sum.ts
+++ 0018-4sum/0018-4sum.ts
@@ -1,24 +1,32 @@
-function fourSum(nums: number[], target: number, length:number = 4): number[][] {
-    if (!length) return target ? [] : [[]];
-    if (nums.length < length) return [];
-    const set = new Set();
-    const num = nums[0];
-    const additives = []
-    const remsWith = fourSum(nums.slice(1), target - nums[0], length-1);
-    for (const rem of remsWith){
-          const valid = [nums[0], ...rem].sort((a,b) => a - b);
-          if (!set.has(valid.toString())){
-              set.add(valid.toString());
-              additives.push(valid);
-          }
-        }
-    const remsWithout = fourSum(nums.slice(1),target,length);
-    for (const rem of remsWithout){
-        const valid = rem.sort((a,b) => a - b);
-        if (!set.has(valid.toString())){
-            set.add(valid.toString());
-            additives.push(rem);
-        }
+function fourSum(nums: number[], target: number): number[][] {
+    nums.sort((a, b) => a - b);
+    return kSum(nums, target, 4);
+}
+
+function kSum(nums: number[], target: number, k: number): number[][] {
+    if (k === 2) return twoSum(nums, target);
+    
+    const result = [];
+    for (let i = 0; i < nums.length - k + 1; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        const subs = kSum(nums.slice(i + 1), target - nums[i], k - 1);
+        for (const sub of subs) result.push([nums[i], ...sub]);
     }
-    return additives;
-};
+    return result;
+}
+
+function twoSum(nums: number[], target: number): number[][] {
+    const result = [];
+    let l = 0, r = nums.length - 1;
+    while (l < r) {
+        const sum = nums[l] + nums[r];
+        if (sum === target) {
+            result.push([nums[l], nums[r]]);
+            while (nums[l] === nums[l+1]) l++;
+            while (nums[r] === nums[r-1]) r--;
+            l++; r--;
+        } else if (sum < target) l++;
+        else r--;
+    }
+    return result;
+}
