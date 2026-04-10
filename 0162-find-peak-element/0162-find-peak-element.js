aa9c4828ed3b9950a9213736c507fa5131a1fcf2
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-09 22:20:38 -0400

    Time: 0 ms (100%), Space: 53.1 MB (88.32%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -1,7 +1,43 @@
 /**
  * @param {number[]} nums
  * @return {number}
+
+returning index of an element thats greater than its neighbors
+
+first and last elements are valid solutions
+
+i = 0
+
+if nums[i] > next && nums[i] > prev return i
+
+
+
+[1,2,1,0,1,2,1] => -1 or any index
+[1,1,1,3,4,2,1,3,5,6,4]
+
+const i = Math.floor((nums.length - 1 - currentIndex) / 2);
+let prev = -Infinity
+let next = -Infinity
+-> if i !== 0 or prev = nums [i-1]
+if i !== length of the array - 1
+    next = nums[i+1]
+
+if next < i slice(i)
+
+i = 0
+
  */
+//  [1,2,1,3,5,6,4]
+// [-1,-1,-1]
+    // for (let i = 0 ; i< nums.length ; i++){
+    //     let prev = -Infinity,
+    //         next = -Infinity;
+    //     if (i !== 0) prev = nums[i-1];
+    //     if (i !== nums.length-1) next = nums[i+1];
+
+    //     if (nums[i] > prev && nums[i] > next) return i;
+    // }
+    // return -1;
 var findPeakElement = function(nums) {
     const mid = Math.floor((nums.length - 1) / 2);
     if (mid === 0 && mid === nums.length - 1) return mid;
