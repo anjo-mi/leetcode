bc6f5f6d7df114ae06a11779facad3e9681906c1
Kyle Nojaim <nojaim.k@gmail.com>
2026-05-17 18:40:29 -0400

    Time: 1 ms (87.64%), Space: 61.8 MB (68.84%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var diameterOfBinaryTree = function(root) {
+    let max = 0;
+    const depth = (r) => {
+        if (!r) return 0;
+        const left = depth(r.left);
+        const right = depth(r.right);
+        max = Math.max(max,left + right);
+        return 1 + Math.max(left,right);
+    }
+    depth(root);
+    return max;
+};
