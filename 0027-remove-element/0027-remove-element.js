0538a2e4359b54e533ea176c7702d35e42e90675
Kyle Nojaim <nojaim.k@gmail.com>
2026-05-17 22:46:24 -0400

    Time: 0 ms (100%), Space: 52.6 MB (96.11%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,23 +4,23 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    // for (let i = 0 ; i < nums.length ; i++){
-    //     if (val === nums[i]){
-    //         nums.splice(i,1);
-    //         i--;
-    //     }
-    // }
-    // return nums.length;
-    let i = 0, j = nums.length - 1;
-    while (i < j){
-        while (nums[i] !== val && i < j) i++;
-        while (nums[j] === val && i < j) j--;
-        if (i < j) [nums[i],nums[j]] = [nums[j],nums[i]];
-        i++;
-        j--;
+    for (let i = 0 ; i < nums.length ; i++){
+        if (val === nums[i]){
+            nums.splice(i,1);
+            i--;
+        }
     }
-    let count = 0;
-    while (nums[count] !== val && count < nums.length) count++;
-    nums.splice(count);
-    return count;
+    return nums.length;
+    // let i = 0, j = nums.length - 1;
+    // while (i < j){
+    //     while (nums[i] !== val && i < j) i++;
+    //     while (nums[j] === val && i < j) j--;
+    //     if (i < j) [nums[i],nums[j]] = [nums[j],nums[i]];
+    //     i++;
+    //     j--;
+    // }
+    // let count = 0;
+    // while (nums[count] !== val && count < nums.length) count++;
+    // nums.splice(count);
+    // return count;
 };
