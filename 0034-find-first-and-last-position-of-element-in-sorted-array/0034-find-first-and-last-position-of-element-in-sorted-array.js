43d958b6efa55f0b5ca772a3f47c12b8272b8dab
Kyle Nojaim <nojaim.k@gmail.com>
2026-05-29 21:46:52 -0400

    Time: 0 ms (100%), Space: 54.2 MB (81.31%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,15 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target,i=0) {
+var searchRange = function(nums, target) {
     if (!nums.length) return [-1,-1];
-    const middle = Math.floor(nums.length / 2);
-    if (nums[middle] > target) return searchRange(nums.slice(0,middle),target,i);
-    if (nums[middle] < target) return searchRange(nums.slice(middle + 1),target,middle + 1 + i);
-    let [l,m] = [middle-1,middle+1];
-    while (nums[l] === nums[middle]) l--;
-    while (nums[m] === nums[middle]) m++;
-    return [l+1+i,m-1+i];
+    let l = 0, r = nums.length - 1;
+    while (l <= r){
+        const mid = Math.floor((l + r) / 2);
+        if (nums[mid] < target) l = mid + 1;
+        else if (nums[mid] > target) r = mid - 1;
+        else{
+            l = mid - 1;
+            r = mid + 1;
+            while (nums[l] === nums[mid]) l--;
+            while (nums[r] === nums[mid]) r++;
+            return [l+1,r-1];
+        }
+    }
+    return [-1,-1];
 };
 
 /**
