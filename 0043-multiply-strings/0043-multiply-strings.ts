daec39aac473eb0fc64ef2a502a4422c25a2de83
Kyle Nojaim <nojaim.k@gmail.com>
2026-06-16 21:55:33 -0400

    Time: 26 ms (17.42%), Space: 61.1 MB (21.97%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.ts
@@ -0,0 +1,19 @@
+function multiply(num1: string, num2: string): string {
+    if (num1 === "0" || num2 === "0") return "0";
+    
+    const m = num1.length, n = num2.length;
+    const pos = new Array(m + n).fill(0);
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            const mul = +num1[i] * +num2[j];
+            const p1 = i + j, p2 = i + j + 1;
+            const sum = mul + pos[p2];
+            pos[p2] = sum % 10;
+            pos[p1] += Math.floor(sum / 10);
+        }
+    }
+    console.log({pos})
+    while (pos[0] === 0) pos.splice(0,1);
+    return pos.join("");
+};
