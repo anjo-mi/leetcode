faf1ee2bfbd3aa6088fd9c9a3ae84ed4091b06d3
Kyle Nojaim <nojaim.k@gmail.com>
2026-06-29 19:35:12 -0400

    Time: 1932 ms (5.37%), Space: 56 MB (23.55%) - LeetHub

--- 0050-powx-n/0050-powx-n.ts
+++ 0050-powx-n/0050-powx-n.ts
@@ -1,1 +1,13 @@
-const myPow = (x: number, n: number): number => x**n;
+const myPow = (x: number, n: number): number => {
+    if (x === 1) return x;
+    if (x=== -1) return n % 2 ? x : -x;
+    let abs = Math.floor(Math.abs(n) / 2);
+    let res = 1;
+    while (abs > 0){
+        res *= x;
+        abs--;
+    }
+    res *= res;
+    if (n % 2 !== 0) res *= x;
+    return n < 0 ? (1 / res) : res;
+}
