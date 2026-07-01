21603db80a554d3846d6608ee4712e1251802cc2
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-01 11:52:33 -0400

    Time: 0 ms (100%), Space: 57.3 MB (5%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.ts
@@ -0,0 +1,51 @@
+const dirs = [
+    [0,1],
+    [1,0],
+    [0,-1],
+    [-1,0],
+]
+
+function spiralOrder(matrix: number[][]): number[] {
+    let bounds: number[] = [1,matrix[0].length-1,0,matrix.length-1];
+    const res:number[] = [];
+    const limit = matrix[0].length * matrix.length;
+    let dir = 0;
+    const place = [0,-1];
+    while (res.length < limit){
+        place[0] += dirs[dir%4][0];
+        place[1] += dirs[dir%4][1];
+        console.log({dir,place})
+        if (dir % 4 === 0){
+            if (place[1] >= bounds[1]){
+                bounds[1]--;
+                dir++;
+            }
+        }else if (dir % 4 === 1){
+            if (place[0] >= bounds[3]){
+                bounds[3]--;
+                dir++;
+            }
+        }else if (dir % 4 === 2){
+            if (place[1] <= bounds[2]){
+                bounds[2]++;
+                dir++;
+            }
+        }else if (dir % 4 === 3){
+            if (place[0] <= bounds[0]){
+                bounds[0]++;
+                dir++;
+            }
+        }
+        res.push(matrix[place[0]][place[1]]);
+    }
+    return res;
+};
+
+[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16],
+    [17,18,19,20],
+    [21,22,23,24]
+]
