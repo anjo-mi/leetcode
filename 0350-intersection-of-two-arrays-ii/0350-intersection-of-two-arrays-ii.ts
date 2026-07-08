188d8935f4d068e577be5f5e39bd232e10dd3553
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-08 13:02:14 -0400

    Time: 4 ms (30.77%), Space: 58.1 MB (38.03%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.ts
@@ -0,0 +1,21 @@
+function intersect(nums1: number[], nums2: number[]): number[] {
+    const res:number[] = [];
+    const t1 = nums1.reduce((a,el) => {
+        a[el.toString()] = (a[el.toString()] || 0) + 1;
+        return a;
+    }, {} as Record<string,number>);
+    const t2 = nums2.reduce((a,el) => {
+        a[el.toString()] = (a[el.toString()] || 0) + 1;
+        return a;
+    }, {} as Record<string,number>);
+    for (const num in t1){
+        if (num in t2){
+            let min = Math.min(t1[num],t2[num]);
+            while (min){
+                res.push(+num);
+                min--;
+            }
+        }
+    }
+    return res;
+};
