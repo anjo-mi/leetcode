62da5daef23722446271446e6ccb0058a00dbdbe
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-08 00:48:19 -0400

    Time: 60 ms (7.06%), Space: 58.3 MB (48.33%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.ts
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -11,8 +11,17 @@
  */
 
 function hasCycle(head: ListNode | null,v=new Set<ListNode>()): boolean {
+    // if (!head) return false;
+    // if (v.has(head)) return true;
+    // v.add(head);
+    // return hasCycle(head.next,v);
     if (!head) return false;
-    if (v.has(head)) return true;
-    v.add(head);
-    return hasCycle(head.next,v);
+    let curr = head,
+        next = head.next;
+    while (next && next.next){
+        if (curr === next) return true;
+        next = next.next.next;
+        curr = curr.next;
+    }
+    return false;
 };
