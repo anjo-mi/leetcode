76bac896fdac48527fccaee25044daba1dd8a9f8
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-18 14:08:48 -0400

    Time: 0 ms (100%), Space: 58.5 MB (57.33%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -10,21 +10,27 @@
  * }
  */
 
-function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    if (!l1) return l2;
-    if (!l2) return l1;
-    const head = Math.min(l1.val,l2.val) === l1.val ? l1 : l2;
-    let curr = head,
-        c1 = head.next,
-        c2 = head === l1 ? l2 : l1;
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    let c1:ListNode | null = list1;
+    let c2:ListNode | null = list2;
+    if (!c1) return c2;
+    if (!c2) return c1;
+    let h:ListNode | null = !c1 ? c2 
+                   : !c2 ? c1 
+                   : c1.val <= c2.val ? c1 : c2;
+    const head:ListNode | null = h;
+    if (c1.val === h.val) c1 = c1.next;
+    else c2 = c2.next;
     while (c1 && c2){
-        const next = Math.min(c1.val,c2.val) === c1.val ? c1 : c2;
-        curr.next = next;
-        curr = curr.next;
-        if (next === c1) c1 = c1.next;
-        if (next === c2) c2 = c2.next;
+        const next:ListNode = !c1 ? c2 
+                   : !c2 ? c1 
+                   : c1.val <= c2.val ? c1 : c2;
+        if (c1?.val === next.val) c1 = c1.next;
+        else c2 = c2.next;
+        h.next = next;
+        h = h.next;
     }
-    if (c1) curr.next = c1;
-    if (c2) curr.next = c2;
+    if (c1) h.next = c1;
+    if (c2) h.next = c2;
     return head;
 };
