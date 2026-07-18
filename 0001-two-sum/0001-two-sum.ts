46f4a73391095eef7addb48645f7f7ef9ef1f3cf
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-18 10:17:21 -0400

    Time: 0 ms (100%), Space: 56.8 MB (57.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,8 +1,10 @@
 function twoSum(nums: number[], target: number): number[] {
+    const possPairs:Record<string,number> = {};
     for (let i = 0 ; i < nums.length ; i++){
-        for (let j = i + 1; j < nums.length ; j++){
-            if (nums[i] + nums[j] === target) return [i,j];
-        }
+        const num = nums[i];
+        const diff = target - num;
+        if (diff in possPairs) return [i,possPairs[diff]];
+        possPairs[num] = i;
     }
     return [-1,-1];
 };
