c531a7a7a2a2b5821de1a77137ccfd3ad5677509
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-18 10:35:08 -0400

    Time: 0 ms (100%), Space: 58.6 MB (23.78%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -1,12 +1,14 @@
 function isValid(s: string): boolean {
-    const opens = {'(' : ')','{' : '}','[' : ']',};
-    const closes = {')' : '(','}' : '{',']' : '[',};
-    const stack = [];
+    const opens = new Set<string>('({[');
+    const closes:Record<string,string> = {  ')':'(' , ']':'[' , '}':'{'  };
+    const stack:string[] = [];
     for (const char of s){
-        if (char in opens) stack.push(char);
+        if (opens.has(char)){
+            stack.push(char);
+        }
         if (char in closes){
-            const openCheck = stack.pop();
-            if (openCheck !== closes[char]) return false;
+            const end = stack.pop();
+            if (!end || end !== closes[char]) return false;
         }
     }
     return !stack.length;
