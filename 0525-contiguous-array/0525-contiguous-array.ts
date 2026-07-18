d8ddcf43098b5c3187a8f9290f7950994b07af04
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-18 00:41:15 -0400

    Time: 46 ms (6.04%), Space: 69.1 MB (5.37%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.ts
@@ -0,0 +1,29 @@
+function findMaxLength(nums: number[]): number {
+    const counts:Record<string,number> = {'0':0, '1':0};
+    const arr:number[] = [];
+    let max:number = 0;
+    for (const num of nums){
+        arr.push(counts['0'] - counts['1']);
+        counts[num]++;
+    }
+    arr.push(counts['0'] - counts['1']);
+    const compares:Record<string,number> = {};
+    for (let i = 0 ; i< arr.length ; i++){
+        const diff = arr[i];
+        if (diff in compares) max = Math.max( i - compares[diff] , max );
+        else compares[diff] = i;
+    }
+    return max;
+};
+
+/**
+counts { 0: 0 , 1 : 0}
+const arr = [];
+max = 0;
+every num, increment our count, push(counts[0] - counts[1])
+const extras = {};
+for every diff of arr
+    if (diff in extras) max = Math.max(extras[diff] - i, max);
+    else extras[diff] = i;
+return max;
+ */
