6b985ac36096da91e9196c2cd286789abcce7e8b
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-19 00:13:07 -0400

    Time: 25 ms (98.15%), Space: 71.9 MB (91.72%) - LeetHub

--- 0015-3sum/0015-3sum.ts
+++ 0015-3sum/0015-3sum.ts
@@ -1,26 +1,51 @@
-const convertToSortedString = (arr:number[]):string => {
-    return String(arr.sort((a,b) => a-b));
-}
-
 function threeSum(nums: number[]): number[][] {
-    const poss = [];
     nums.sort((a,b) => a-b);
+    const res:number[][] = [];
+
     for (let i = 0 ; i < nums.length ; i++){
-        if (i > 0 && nums[i] === nums[i-1]) continue;
-        const tar = -nums[i];
-        let left = i+1,
-            right = nums.length-1;
-        while (left < right){
-            const currSum = nums[left] + nums[right];
-            if (currSum === tar){
-                poss.push([nums[i],nums[left], nums[right]])
-                while (left < right && nums[left] === nums[left+1]) left++;
-                while (left < right && nums[right] === nums[right-1]) right--;
-                left++;
-                right--;
-            }else if(currSum < tar) left++;
-            else right--;
+        if (nums[i] === nums[i-1]) continue;
+        if (nums[i] > 0) break;
+
+        let l = i + 1;
+        let h = nums.length - 1;
+        while (l < h){
+            const sum = nums[i] + nums[l] + nums[h];
+            if (sum < 0) l++;
+            else if (sum > 0) h--;
+            else{
+                res.push([nums[i],nums[l],nums[h]]);
+                while (l < h && nums[l] === nums[l+1]) l++;
+                while (l < h && nums[h] === nums[h-1]) h++;
+                l++;
+                h--;
+            }
         }
     }
-    return poss;
+    return res;
 };
+    // const res = new Set<string>();
+    // for (let i = 0 ; i < nums.length ; i++){
+    //     for (let j = i + 1 ; j < nums.length ; j++){
+    //         for (let k = j + 1 ; k < nums.length ; k++){
+    //             const [a,b,c]: [number,number,number] = [nums[i] , nums[j] , nums[k]]
+    //             if (a + b + c === 0) res.add([a,b,c].sort().join(';'));
+    //         }
+    //     }
+    // }
+    // return Array.from(res).map(el => el.split(';').map(Number));
+    // ------------------------------------------------------------
+    // const res:Set<string> = new Set();
+    // for (let i = 0 ; i< nums.length ;i++){
+    //     const target = -nums[i];
+    //     const seen:Set<number> = new Set();
+    //     for (let j = i + 1 ; j < nums.length ; j++){
+    //         const num = nums[j];
+    //         const diff = target - num;
+    //         if (seen.has(diff)) {
+    //             res.add( [nums[i],num,diff].sort().toString() )
+    //             continue;
+    //         };
+    //         seen.add(num);
+    //     }
+    // }
+    // return Array.from(res).map(r => r.split(',').map(Number));
