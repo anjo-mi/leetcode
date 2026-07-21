10d733093704e45910f7270a300fda6680dcc761
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-21 01:01:50 -0400

    Time: 7 ms (88.81%), Space: 55.7 MB (97.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,3 +1,15 @@
+const getCounts = (s:string): Record<string,number> => {
+    return s.split('').reduce((a,el) => {
+        a[el] = (a[el] || 0) + 1;
+        return a;
+    }, {} as Record<string,number>)
+}
+
 function isAnagram(s: string, t: string): boolean {
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    // return s.split('').sort().join('') === t.split('').sort().join('');
+    const a = getCounts(s);
+    const b = getCounts(t);
+    for (const char in a) if (a[char] !== b[char]) return false;
+    for (const char in b) if (a[char] !== b[char]) return false;
+    return true;
 };
