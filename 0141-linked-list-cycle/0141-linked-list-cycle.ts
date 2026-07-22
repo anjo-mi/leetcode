8010b837b0c09b67e9909f4ef4e150c1c80d0a21
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-22 00:11:37 -0400

    Time: 48 ms (63.89%), Space: 58.8 MB (22.77%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.ts
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -10,18 +10,14 @@
  * }
  */
 
-function hasCycle(head: ListNode | null,v=new Set<ListNode>()): boolean {
-    // if (!head) return false;
-    // if (v.has(head)) return true;
-    // v.add(head);
-    // return hasCycle(head.next,v);
+function hasCycle(head: ListNode | null): boolean {
     if (!head) return false;
-    let curr = head,
-        next = head.next;
+    let curr:ListNode | null = head;
+    let next:ListNode | null = head.next;
     while (next && next.next){
-        if (curr === next) return true;
-        next = next.next.next;
+        if (curr === next) return true
         curr = curr.next;
+        next = next.next.next;
     }
     return false;
 };
