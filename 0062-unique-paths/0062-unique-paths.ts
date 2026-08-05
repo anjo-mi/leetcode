1ab4399a937713c808d0ce5070f4d42db71b8eb2
Kyle Nojaim <nojaim.k@gmail.com>
2026-08-05 00:13:06 -0400

    Time: 7 ms (6.71%), Space: 58 MB (5.52%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -0,0 +1,7 @@
+function uniquePaths(m: number, n: number, r:number = 1, c:number = 1, memo:Record<string,number>={}): number {
+    const key = r + ';' + c;
+    if (key in memo) return memo[key];
+    if (r > m || c > n) return memo[key] = 0;
+    if (r === m && c === n) return memo[key] = 1;
+    return memo[key] = uniquePaths(m,n,r+1,c,memo) + uniquePaths(m,n,r,c+1,memo);
+};
