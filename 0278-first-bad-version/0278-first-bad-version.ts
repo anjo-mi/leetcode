abf95598cc934a8a0ea60039b541398e81cff6a1
Kyle Nojaim <nojaim.k@gmail.com>
2026-08-06 19:50:30 -0400

    Time: 2686 ms (5.1%), Space: 55.4 MB (22.27%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.ts
@@ -0,0 +1,14 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function(isBadVersion: any) {
+
+    return function(n: number): number {
+        while (isBadVersion(n)) n--;
+        return n+1;
+    };
+};
