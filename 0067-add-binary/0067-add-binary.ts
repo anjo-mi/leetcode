ffc750950917b29f467469b6c786f4dbca99a015
Kyle Nojaim <nojaim.k@gmail.com>
2026-08-10 00:32:12 -0400

    Time: 0 ms (100%), Space: 55.6 MB (89.41%) - LeetHub

--- 0067-add-binary/0067-add-binary.ts
+++ 0067-add-binary/0067-add-binary.ts
@@ -1,5 +1,3 @@
 function addBinary(a: string, b: string): string {
-    if (!a.length || ! b.length) return '0';
-    const total:bigint = BigInt(`0b${a}`) + BigInt(`0b${b}`);
-    return total.toString(2);
+    return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
 };
