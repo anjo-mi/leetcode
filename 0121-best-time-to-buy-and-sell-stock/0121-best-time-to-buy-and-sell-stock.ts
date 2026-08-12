185d0283ffa86ad5724a364175efa091c7873f73
Kyle Nojaim <nojaim.k@gmail.com>
2026-08-12 13:32:27 -0400

    Time: 1 ms (94.7%), Space: 65.7 MB (69.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,10 +1,10 @@
 function maxProfit(prices: number[]): number {
-    let max = 0;
-    let min = prices[0];
+    let maxProf:number = 0;
+    let minCost:number = prices[0];
     for (let i = 1 ; i < prices.length ; i++){
-        const sell = prices[i];
-        if (sell - min > max) max = sell - min;
-        if (sell < min) min = sell;
+        const sellPrice:number = prices[i];
+        if (sellPrice - minCost > maxProf) maxProf = sellPrice - minCost;
+        if (sellPrice < minCost) minCost = sellPrice;
     }
-    return max;
+    return maxProf;
 };
