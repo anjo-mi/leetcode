9654a21fd5be80ea72444dcc2345b375b7424dc8
Kyle Nojaim <nojaim.k@gmail.com>
2026-08-18 00:02:26 -0400

    Time: 32 ms (99.51%), Space: 71.9 MB (99.61%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.ts
+++ 0146-lru-cache/0146-lru-cache.ts
@@ -24,9 +24,3 @@ class LRUCache {
     }
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
