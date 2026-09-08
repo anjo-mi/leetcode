49342e6933cf4301cdb33486a017b58e1ea5585a
Kyle Nojaim <nojaim.k@gmail.com>
2026-09-08 00:14:49 -0400

    Time: 1 ms (19.45%), Space: 55.5 MB (49.49%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
@@ -0,0 +1,15 @@
+function search(nums: number[], target: number): number {
+    let i = 0 , j = nums.length - 1;
+    while (i <= j){
+        const mid = Math.floor((i + j) / 2);
+        if (nums[mid] === target) return mid;
+        if (nums[i] <= nums[mid]) {
+            if (nums[i] <= target && target < nums[mid]) j = mid - 1;
+            else i = mid + 1;
+        } else {
+            if (nums[mid] < target && target <= nums[j]) i = mid + 1;
+            else j = mid - 1;
+        }
+    }
+    return -1;
+};
